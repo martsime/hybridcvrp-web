fb5fad5af97faff49919475448ca193913969574
Martin Simensen <martsime@users.noreply.github.com>
2021-08-26 12:28:41 +0200

    Add polyfill for replaceAll

--- src/instances.js
+++ src/instances.js
@@ -1,3 +1,19 @@
+// Polyfill for replaceAll
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str, newStr) {
+
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr);
+    }
+
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+
+  };
+}
+
+
 function parseInstance(text) {
 
   function parseProperty(line) {
